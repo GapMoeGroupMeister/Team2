1eade0ce9fd8bbcff6cecaba5afe648e6fd4cbe3
djEjsdlfma <asdakpol.asd@gmail.com>
2024-04-01 00:05:41 +0900

    feat : DayCycle

---
+++ Assets/01.Scripts/Core/DayCycle/DayTimer.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class DayTimer : MonoBehaviour
+{
+    private Slider Timer;
+    private float remainTime;
+
+    private void start()
+    {
+        Timer = GetComponent<Slider>();
+    }
+
+    private void Update()
+    {
+        remainTime += Time.deltaTime;
+        Timer.value = remainTime;
+
+        if (remainTime == 5)
+        {
+            Time.timeScale = 0f;
+        }
+    }
+}
--- Assets/01.Scripts/Item/WeaponClass/WeaponClass.cs
+++ Assets/01.Scripts/Item/WeaponClass/WeaponClass.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class WeaponClass : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public bool isSowrd;
+    public bool isSpear;
+    public bool isAxe;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
+
 }
+
+/*
+    - 검 :  빠름 / /사정거리 중간/ 공격범위 보통
+    - 창 :  매우 빠름 / /사정거리 중간 / 공격범위 작음
+    - 도끼 : 보통 / / 사정거리 중간 / 공격 범위 큼
+    - 둔기류 :  공속 느림 / 중장갑병에게 추가피해 /사정거리 중간/공격 범위 매우 큼 (한바퀴 돌듯이 공격)
+    - 원거리 무기(활, 석궁 등) : 보통 / 조준 시 이속 느려짐 /사정거리 김/ 공격 범위 작음
+    - 방패 : 앞에 오는 공격 방어 가능
+*/
