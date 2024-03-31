3a3476cd640eca15f08579f8be348d78d8f1f265
san232 <ebs00098@gmail.com>
2024-04-01 00:46:25 +0900

    style : InteractionObj

--- Assets/01.Scripts/Item/Interaction/InteractionObject.cs
+++ Assets/01.Scripts/Item/Interaction/InteractionObject.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class InteractionObject : InteractionObjType
 {
 
     private PlayerMovement Galahad;
-    private GameObject playerObject;
+    public GameObject playerObject;
+    public GameObject Scrap;
 
     private void Awake()
     {
@@ -16,15 +18,15 @@ public class InteractionObject : InteractionObjType
 
     private void Update()
     {
-
+        
     }
 
-    private void OnTriggerEnter2D(Collider2D other)
+    private void OnTriggerStay2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
             string tagValue = gameObject.tag;
-            
+
 
             if (playerObject != null)
             {
@@ -32,21 +34,36 @@ public class InteractionObject : InteractionObjType
                 if (playerMovement != null)
                 {
                     playerMovement.SetTagValue(tagValue);
-                    Search();
+                    Debug.Log(tagValue);
+
                 }
-                
+
             }
-            
+
         }
     }
 
+
+    protected void Search()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector2 worldPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            Collider2D coll = Physics2D.OverlapCircle(worldPoint, 0.5f);
+
+            if (coll == Scrap)
+            {
+                PickedUp();
+            }
+        }
+    }
+
+    
     protected override void Interaction()
     {
 
     }
 
-    protected override void Search()
-    {
-        
-    }
+    
 }
