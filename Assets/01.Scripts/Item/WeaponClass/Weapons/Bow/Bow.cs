308f6102bb06078048e98e65ec856d07d3be196d
san232 <ebs00098@gmail.com>
2024-04-25 01:49:37 +0900

    feat : bowLaunch

--- Assets/01.Scripts/Item/WeaponClass/Weapons/Bow/Bow.cs
+++ Assets/01.Scripts/Item/WeaponClass/Weapons/Bow/Bow.cs
@@ -6,14 +6,38 @@ public class Bow : WeaponManager
 {
     [SerializeField] private GameObject arrowPrefab;
     [SerializeField] private GameObject bowPos;
-
     [SerializeField] private float _bowRange = 8.4f; // Ã¢ÀÇ 2¹èÂë
+    [SerializeField] private float _maxBowRange = 16.8f;
     [SerializeField] private float _bowAttackDamage = 8f;
+    [SerializeField] private float _maxBowAttackDamage = 32f;
     [SerializeField] private float _bowAttackSpeed = 2f;
+    private bool pew = false;
 
     private void Update()
     {
-        GameObject arrow = Instantiate(arrowPrefab);
-        arrow.transform.position = bowPos.transform.position;
+        if (_bowRange <= _maxBowRange || _bowAttackDamage <= _maxBowAttackDamage)
+        {
+            pew = true;
+        }
+
+        if(Input.GetMouseButton(0))
+        {
+            switch (pew)
+            {
+                case true:
+                    GameObject arrow = Instantiate(arrowPrefab);
+                    arrow.transform.position = bowPos.transform.position;
+                    break;
+                case false:
+                    _bowRange += Time.deltaTime;
+                    _bowAttackDamage += Time.deltaTime * 2;
+                    break;
+            }
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+
+        }
+        
     }
 }
